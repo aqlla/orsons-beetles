0721c19f00c8bd61e25ecd03b0521f6f200e0131
Aquilla Sherrock <acsherrock@gmail.com>
2024-09-29 20:55:02 -0700

    build - decided to move to another repo for the next steps. THis will remain the world viewer

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/orsons-beetles/',
-  assetsInclude: ['geometries/*.json'],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        worldview: resolve(__dirname, 'worldview/index.html'),
-      },
-    },
-  },
+  assetsInclude: ['geometries/*.json']
 })
