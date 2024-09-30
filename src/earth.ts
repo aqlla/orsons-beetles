972f6701546d4ccf490e58f9b781ad5f942eb6ea
Aquilla Sherrock <acsherrock@gmail.com>
2024-09-29 17:07:07 -0700

    fasflijaslfije 2

--- src/earth.ts
+++ src/earth.ts
@@ -222,7 +222,7 @@ const makeEarthMeshes = (tiles: Array<Tile>, radius: number) => {
 type EarthMeshes = Record<string, THREE.Object3D>
 
 export const getEarth = (n: number, r: number): Promise<EarthMeshes> =>
-	fetch(`geometries/goldberg_${n}_${r}.json`)
+	fetch(`/geometries/goldberg_${n}_${r}.json`)
 		.then(res => {
 			console.log(res)
 			return res
